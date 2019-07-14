1548e9c54260d3daf801b2b26097383baa038531
= <=>
2019-07-15 00:24:02 +0700

    init project

---
+++ src/as/add.go
@@ -0,0 +1,10 @@
+package as
+
+import "fmt"
+
+func Add(a, b int) int {
+	return  a+b
+}
+func In()  {
+	fmt.Print("ssdsdsdsd")
+}
---
+++ src/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"as"
+	"fmt"
+)
+//import "src/as"
+
+var a, b int = 6, 7
+
+func main()  {
+	fmt.Print("aaddd", as.Add(a,b))
+	fmt.Print("aaaaa")
+	as.In()
+}
